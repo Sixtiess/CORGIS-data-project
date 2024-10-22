ff63a3bd070247c873fa2174c89902810e08579e
sixtiess <liamhhickey@icloud.com>
2024-10-22 12:41:53 -0700

    Move over from research to main project

--- webapp.py
+++ webapp.py
@@ -1,54 +1,75 @@
 from flask import Flask, request, render_template, flash
 from markupsafe import Markup
+from datetime import datetime
 
 import os
 import json
-import math
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    states = get_state_options()
-    #print(states)
-    return render_template('home.html', state_options=states)
+def render_about():
+    return render_template('home.html')
 
-@app.route('/showFact')
-def render_fact():
-    states = get_state_options()
-    state = request.args.get('state')
-    county = county_with_highest_vets_percentage(state)
-    county2 = county_most_under_18(state)
-    fact = "In " + state + ", the county with the highest percentage of veterans is " + county + " at "+ get_county_with_highest_vets_percentage(state,0)+"%."
-    fact2 = "Also, in " + state + ", the county with the highest percentage of people under 18 is " + county2 + "."
-    return render_template('home.html', state_options=states, funFact=fact, funFact2=fact2)
+@app.route('/delaybyairport')
+def render_delaybyairport():
+    options = ""
+    with open('airlines.json') as airlines_data:
+        months = json.load(airlines_data)
+        
+    if "airport" in request.args:
+        airport = request.args['airport']
+        
+        maxNum = 0
+        numMonth = ""
+        maxMinutes = 0
+        minutesMonth = ""
+        for i in months:
+            if i["Airport"]["Code"] == airport:
+                if i["Statistics"]["Minutes Delayed"]["Total"] > maxMinutes:
+                    maxMinutes = i["Statistics"]["Minutes Delayed"]["Total"]
+                    minutesMonth = i["Time"]["Month Name"]+" of "+str(i["Time"]["Year"])
+                if i["Statistics"]["Flights"]["Delayed"] > maxNum:
+                    maxNum = i["Statistics"]["Flights"]["Delayed"]
+                    numMonth = i["Time"]["Month Name"]+" of "+str(i["Time"]["Year"])
+        for i in range(0,152):
+            options += Markup("<option value=\"" + months[i]["Airport"]["Code"] + "\">" + months[i]["Airport"]["Name"] + "</option>")
+        return render_template('mostDelayedMonthResponse.html', options=options,maxNum1=maxNum,numMonth1=numMonth,maxMinutes1=maxMinutes,minutesMonth1=minutesMonth)
     
-def get_state_options():
-    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    states=[]
-    for c in counties:
-        if c["State"] not in states:
-            states.append(c["State"])
-    options=""
-    for s in states:
-        options += Markup("<option value=\"" + s + "\">" + s + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
-    return options
+    for i in range(0,29):
+        options += Markup("<option value=\"" + months[i]["Airport"]["Code"] + "\">" + months[i]["Airport"]["Name"] + "</option>")
+    return render_template("mostDelayedMonth.html",options=options)
+    
 
-def county_most_under_18(state):
-    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
-    with open('demographics.json') as demographics_data:
-        counties = json.load(demographics_data)
-    highest=0
-    county = ""
-    for c in counties:
-        if c["State"] == state:
-            if c["Age"]["Percent Under 18 Years"] > highest:
-                highest = c["Age"]["Percent Under 18 Years"]
-                county = c["County"]
-    return county
-
+@app.route('/delaybytime')
+def render_delaybytime():
+    options = ""
+    with open('airlines.json') as airlines_data:
+        months = json.load(airlines_data)
+        
+    if "month" in request.args:
+        month = request.args['month']
+        
+        maxNum = 0
+        numAirport = ""
+        maxMinutes = 0
+        minutesAirport = ""
+        for i in months:
+            if i["Time"]["Label"] == month:
+                if i["Statistics"]["Minutes Delayed"]["Total"] > maxMinutes:
+                    maxMinutes = i["Statistics"]["Minutes Delayed"]["Total"]
+                    minutesAirport = i["Airport"]["Name"]
+                if i["Statistics"]["Flights"]["Delayed"] > maxNum:
+                    maxNum = i["Statistics"]["Flights"]["Delayed"]
+                    numAirport = i["Airport"]["Name"]
+        for i in range(0,152):
+            options += Markup("<option value=\"" + months[i*29]["Time"]["Label"] + "\">" + months[i*29]["Time"]["Label"] + "</option>")
+        return render_template('mostDelayedAirportResponse.html', options=options,maxNum1=maxNum,numAirport1=numAirport,maxMinutes1=maxMinutes,minutesAirport1=minutesAirport)
+    
+    for i in range(0,152):
+        options += Markup("<option value=\"" + months[i*29]["Time"]["Label"] + "\">" + months[i*29]["Time"]["Label"] + "</option>")
+    return render_template("mostDelayedAirport.html",options=options)
+ 
 def is_localhost():
     """ Determines if app is running on localhost or not
     Adapted from: https://stackoverflow.com/questions/17077863/how-to-see-if-a-flask-app-is-being-run-on-localhost
@@ -57,6 +78,6 @@ def is_localhost():
     developer_url = 'http://127.0.0.1:5000/'
     return root_url == developer_url
 
-    
+
 if __name__ == '__main__':
-    app.run(debug=False) # change to False when running in production
+    app.run(debug=False)
